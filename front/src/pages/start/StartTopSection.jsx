974b9a1dfd134c2e425f91e5ef540368d93f3913
hun0711 <sh6351@naver.com>
2023-08-28 20:10:51 +0900

    0828

--- front/src/pages/start/StartTopSection.jsx
+++ front/src/pages/start/StartTopSection.jsx
@@ -4,52 +4,52 @@ import Typography from "../../utils/typography";
 import StartTopSectionLayout from "./StartTopSectionLayout";
 import SouthIcon from '@mui/icons-material/South';
 
-const backgroundImage = "/images/배경움짤.gif";
+const backgroundVideo = '/videos/sfdb.mp4'
 
   
 export default function StartTopSection() {
   return (
     <StartTopSectionLayout
       sxBackground={{
-        backgroundImage: `url(${backgroundImage})`,
-        backgroundColor: "#7fc7d9", // Average color of the background image.
+        backgroundImage: `none`,
+        backgroundColor: "#7fc7d9", 
         backgroundPosition: "center",
       }}
     >
-      {/* Increase the network loading priority of the background image. */}
-      <img
-        style={{ display: "none" }}
-        src={backgroundImage}
-        alt="SF-DB 첫화면 이미지"
-      />
-      <Typography
-        color="white"
-        align="center"
-        variant="h2"
-        marked="center"
-        marginTop="50px"
-        marginBottom="-50px"
-      >
-        온 우주의 SF 영화를 담다
-      </Typography>
-      <Typography
-        color="science"
-        align="center"
-        variant="h5"
-        sx={{ mb: 4, mt: { xs: 4, sm: 10 } }}
-      >
-        상상력을 자극하는 작품들이 기다리고 있습니다
-      </Typography>
-      <Button
-        color="science"
-        variant="contained"
-        size="large"
-        component="a"
-        href="/main"
-        sx={{ minWidth: 200 }}
-      >
-        둘러보기
-      </Button>
+   <video autoPlay loop muted style={{ width: "1500px", height: "650px", marginBottom:'145px' ,objectFit: "cover" , opacity:'60%' }}>
+        <source src={backgroundVideo} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div style={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+        <Typography
+          color="white"
+          align="center"
+          variant="h2"
+          marked="center"
+          marginTop="50px"
+          marginBottom="-50px"
+        >
+          온 우주의 SF 영화를 담다
+        </Typography>
+        <Typography
+          color="science"
+          align="center"
+          variant="h5"
+          sx={{ mb: 4, mt: { xs: 4, sm: 10 } }}
+        >
+          상상력을 자극하는 작품들이 기다리고 있습니다
+        </Typography>
+        <Button
+          color="science"
+          variant="contained"
+          size="large"
+          component="a"
+          href="/main"
+          sx={{ minWidth: 200 }}
+        >
+          둘러보기
+        </Button>
+      </div>
       <Typography variant="body2" color="inherit" sx={{ mt: 2 }}>
         Discover the experience
       </Typography>
